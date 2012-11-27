9dd980cd83af0119f1008a73e4ebebb216017d96
Alex Shulgin <ash@commandprompt.com>
2012-11-27 13:21:01 -0800

    Redmine-2.x compatibility.

--- init.rb
+++ init.rb
@@ -1,27 +1,26 @@
 require 'redmine'
-require 'dispatcher'
-
-require 'redmine_silencer/issue_hooks'
-require 'redmine_silencer/view_hooks'
-
-Dispatcher.to_prepare :redmine_silencer do
-  require_dependency 'journal'
-  require_dependency 'journal_observer'
-
-  # Redmine-1.1 compatibility
-  unless Journal.new.respond_to? :notify?
-    Journal.send(:include, RedmineSilencer::JournalPatch)
-    JournalObserver.send(:include, RedmineSilencer::JournalObserverPatch)
-  end
-end
 
 Redmine::Plugin.register :redmine_silencer do
   name 'Redmine Silencer plugin'
   author 'Alex Shulgin <ash@commandprompt.com>'
   description 'A Redmine plugin to suppress issue email notifications.'
-  version '0.1.0'
+  version '0.2.0'
   url 'https://github.com/commandprompt/redmine_silencer'
-  requires_redmine '1.1.0'
+  requires_redmine :version_or_higher => '2.0.x'
 
   permission :suppress_mail_notifications, {}
 end
+
+prepare_block = Proc.new do
+  Journal.send(:include, RedmineSilencer::JournalPatch)
+  JournalObserver.send(:include, RedmineSilencer::JournalObserverPatch)
+end
+
+if Rails.env.development?
+  ActionDispatch::Reloader.to_prepare { prepare_block.call }
+else
+  prepare_block.call
+end
+
+require 'redmine_silencer/issue_hooks'
+require 'redmine_silencer/view_hooks'
