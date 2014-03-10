98ea6fb101fafc2e9dafe33c09e898de864c1237
Tobias Fischer <tobias@mediaversal.de>
2014-03-10 21:03:58 +0100

    Remove JournalObserver for compatibility with redmine 2.4
    Also: version bumped to 0.3.0

--- init.rb
+++ init.rb
@@ -2,10 +2,11 @@ require 'redmine'
 
 Redmine::Plugin.register :redmine_silencer do
   name 'Redmine Silencer plugin'
-  author 'Alex Shulgin <ash@commandprompt.com>'
+  author 'Alex Shulgin'
   description 'A Redmine plugin to suppress issue email notifications.'
-  version '0.2.0'
+  version '0.3.0'
   url 'https://github.com/commandprompt/redmine_silencer'
+  author_url 'https://github.com/commandprompt'
   requires_redmine :version_or_higher => '2.0.x'
 
   permission :suppress_mail_notifications, {}
@@ -13,7 +14,6 @@ end
 
 prepare_block = Proc.new do
   Journal.send(:include, RedmineSilencer::JournalPatch)
-  JournalObserver.send(:include, RedmineSilencer::JournalObserverPatch)
 end
 
 if Rails.env.development?
