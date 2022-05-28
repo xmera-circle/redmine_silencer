266aba8ffddb32a9335aaa8b9daef0b2ff09b229
Liane Hampe <liaham@xmera.de>
2022-05-28 22:05:25 +0200

    Adds email suppress box for new issues

--- lib/redmine_silencer/view_hooks.rb
+++ lib/redmine_silencer/view_hooks.rb
@@ -1,5 +1,15 @@
 module RedmineSilencer
   class ViewHooks < Redmine::Hook::ViewListener
+    def view_issues_form_details_bottom(context = {})
+      return unless /^(Issues)/.match?(context[:controller].class.name)
+      return unless context[:controller].action_name == 'new'
+
+      context[:controller].send :render_to_string, {
+        partial: 'hooks/silencer_suppress_mail',
+        locals: context
+      }
+    end
+
     render_on :view_issues_edit_notes_bottom,
       :partial => 'hooks/silencer_suppress_mail'
 
